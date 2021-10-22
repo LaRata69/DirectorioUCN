cdab4b828d46a7f8a182a7e15f7b64a46830c7be
Ronald <rcp019@alumnos.ucn.cl>
2021-10-22 11:49:11 -0300

    build(gradle): Add more ACRA core config

--- app/src/main/java/cl/ucn/disc/dsm/rcampillay/directorioucn/App.java
+++ app/src/main/java/cl/ucn/disc/dsm/rcampillay/directorioucn/App.java
@@ -5,6 +5,8 @@ import android.content.Context;
 
 import org.acra.ACRA;
 import org.acra.annotation.AcraCore;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.data.StringFormat;
 
 /**
  * Main app
@@ -21,6 +23,9 @@ public class App extends Application{
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+
+        CoreConfigurationBuilder builder = new CoreConfigurationBuilder(this);
+        builder.withBuildConfigClass(BuildConfig.class).withReportFormat(StringFormat.JSON).withEnabled(true);
 
         // The following Line triggers the initialization of ACRA
         ACRA.init(this);
